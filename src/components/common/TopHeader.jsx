fac870c214b1df59bd5cf31bb5f301017f9bb618
hasibul-99 <hasibul_uap@outlook.com>
2024-01-07 06:52:05 +0600

    new formate 7-1-24

--- src/components/common/TopHeader.jsx
+++ src/components/common/TopHeader.jsx
@@ -1,15 +1,6 @@
 import React from 'react'
 import { FaFacebookF, FaInstagram, FaLinkedin } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
-import { Card, CardContent } from "@/components/ui/card"
-import {
-    Carousel,
-    CarouselContent,
-    CarouselItem,
-    CarouselNext,
-    CarouselPrevious,
-} from "@/components/ui/carousel"
-
 
 
 export default function TopHeader() {
@@ -57,24 +48,7 @@ export default function TopHeader() {
                                     </div>
                                 </div>
                             </div> */}
-                            <Carousel className="w-full max-w-xs">
-                                <CarouselContent>
-                                    {Array.from({ length: 5 }).map((_, index) => (
-                                        <CarouselItem key={index}>
-                                            <div className="p-1">
-                                                <Card>
-                                                    
-                                                    <CardContent className="flex aspect-square items-center justify-center p-6">
-                                                        <span className="text-4xl font-semibold">{index + 1}</span>
-                                                    </CardContent>
-                                                </Card>
-                                            </div>
-                                        </CarouselItem>
-                                    ))}
-                                </CarouselContent>
-                                <CarouselPrevious />
-                                <CarouselNext />
-                            </Carousel>
+                            
                         </div>
                     </div>
                     <div className="col-lg-4">
