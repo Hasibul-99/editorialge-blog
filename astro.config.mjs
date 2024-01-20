1561c06457974d3b8a979fb272b823faf30fd49d
hasibul-99 <hasibul_uap@outlook.com>
2024-01-20 21:43:13 +0600

    language chnage process implement

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import {
 
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import astroI18next from "astro-i18next";
 
 
 // https://astro.build/config
@@ -17,6 +18,7 @@ export default defineConfig({
     },
   },
   integrations: [
+    astroI18next(),
     react(),
     tailwind({
       applyBaseStyles: false,
