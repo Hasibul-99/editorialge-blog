b4388d4a9b4bbaafbd3ad074287bd48a95a79144
hasibul-99 <hasibul_uap@outlook.com>
2024-01-25 00:07:15 +0600

    darckmode and scroll top implement

--- src/components/common/DarkLightToggle.tsx
+++ src/components/common/DarkLightToggle.tsx
@@ -1,12 +1,44 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 type Props = {}
 
 export default function DarkLightToggle({ }: Props) {
+    const handelModeChange = () => {
+        console.log("hello");
+        
+        if (localStorage.getItem('tg_theme_scheme') === 'dark') {
+            tg_set_scheme('light');
+        } else {
+            tg_set_scheme('dark');
+        }
+    }; 
+
+    const tg_set_scheme = (tg_theme: string) => {
+        localStorage.setItem('tg_theme_scheme', tg_theme);
+        document.documentElement.setAttribute("tg-theme", tg_theme);
+    }
+
+    const tg_init_theme = () => {
+        let element : HTMLInputElement | null = document.querySelector('.modeSwitch');
+
+        if (element) {
+            if (localStorage.getItem('tg_theme_scheme') === 'dark') {
+                tg_set_scheme('dark');
+                element.checked = true;
+            } else {
+                tg_set_scheme('light');
+                element.checked = false;
+            }
+        }
+    }
+    useEffect(() => {
+        tg_init_theme();
+    },[])
+
     return (
         <div className="darkmode-trigger">
-            <label className="modeSwitch">
-                <input type="checkbox" />
+            <label className="modeSwitch" >
+                <input type="checkbox" onChange={() => handelModeChange()}/>
                 <span className="icon" />
             </label>
         </div>
--- src/components/common/ScrollTop.tsx
+++ src/components/common/ScrollTop.tsx
@@ -3,8 +3,15 @@ import React from 'react'
 type Props = {}
 
 export default function ScrollTop({ }: Props) {
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+    };
+
     return (
-        <button id="scroll-to-target" className="scroll-top scroll-to-target" data-target="html">
+        <button onClick={() => scrollToTop()} id="scroll-to-target" className="scroll-top scroll-to-target" data-target="html">
             <i className="fas fa-angle-up" />
         </button>
     )
